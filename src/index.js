7857d4a6143f6390de57fbc56174841dd17c42c5
enerjaizzzer <enerjaizzzer@gmail.com>
2019-03-14 12:36:58 +0300

    exp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var dividers = [];
+  var powers = [];
+
+  for (var i = base / 2; i >= 2; i--) {
+
+      var power = 0;
+
+      while (base % i === 0) {
+          base /= i;
+          power++;
+      }
+
+      if (power > 0) {
+          dividers.push(i);
+          powers.push(power);
+      }
+  }
+  console.log(powers);
+  console.log(dividers);
 }
